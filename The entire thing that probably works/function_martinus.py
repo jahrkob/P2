2d540e6f2c70e6471758aaecfe665b7bb7e928ea
Marvirk05 <234126316+Marvirk05@users.noreply.github.com>
2026-04-22 13:59:52 +0200

    Added amr functions

---
+++ The entire thing that probably works/function_martinus.py
@@ -0,0 +1,53 @@
+import sqlite3
+import json
+import time
+from datetime import datetime
+import requests
+
+class NetworkMonitorer:
+    """Class to monitor the network and manage the fleet of AMRs."""
+
+    def __init__(self, fleet_manager_ip, database = "database.db", auth_token = None):
+        self.fleet_manager_ip = fleet_manager_ip
+        self.database = database
+        self.auth_token = auth_token
+        self.amr_list = []
+
+        self.initialize_database()
+        self.load_devices_from_database()
+
+    def __str__(self):
+        amr_info = "\n".join(str(amr) for amr in self.amr_list)
+        return (
+            f"Fleet Manager IP: {self.fleet_manager_ip}\n\n"
+            f"AMRs:\n{amr_info if amr_info else 'Ingen AMR fundet'}"
+        )
+
+    def add_amr_to_database(self, id, ip, name, raspi_ip):
+        conn = sqlite3.connect("test_database.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO amr (id, ip, name, raspi_ip) VALUES (?, ?, ?, ?)", 
+            (id, ip, name, raspi_ip)
+        )
+
+        conn.commit() # MANGLER I APPLICATION KODEN
+        conn.close()
+
+    def remove_amr_from_database(self, id):
+        conn = sqlite3.connect("test_database.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "DELETE FROM amr WHERE id = ?", (id,)
+        )
+
+        conn.commit()
+        conn.close()
+
+    def save_network_data(self):
+        pass
+
+    def save_amr_status_log(self):
+        pass
