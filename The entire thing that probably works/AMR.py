9b19a05318db49843067f3adc0db0e33477189fe
Nestasuo <nestasuo@gmail.com>
2026-04-22 12:56:51 +0200

    Update AMR.py

--- The entire thing that probably works/AMR.py
+++ The entire thing that probably works/AMR.py
@@ -1,8 +1,8 @@
 import requests
 
-class MiR200:
-    def __init__(self):
-        self.base_url = "http://192.168.100.51/api/v2.0.0"
+class AMR:
+    def __init__(self, ip):
+        self.base_url = f"http://{ip}/api/v2.0.0"
         self.headers = {
             "Authorization": "Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGnmOTI5NzkyODM0YjAxNA==",
             "Accept-Language": "en-US",
@@ -35,4 +35,4 @@ class MiR200:
         else:
             print(f"Error {response.status_code}: {response.text}")
 
-MiR200.get_status()
+AMR.get_status()
