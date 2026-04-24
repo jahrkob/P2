ce975da615cbcb4db713bd8833b614a1f23ee0c6
Marvirk05 <234126316+Marvirk05@users.noreply.github.com>
2026-04-24 09:26:38 +0200

    Added functions (save_network_data, save_amr_status_log) + updated remove_amr_from_database

--- The entire thing that probably works/function_martinus.py
+++ The entire thing that probably works/function_martinus.py
@@ -36,18 +36,50 @@ class NetworkMonitorer:
         conn.close()
 
     def remove_amr_from_database(self, id):
+        """Removes an AMR from all tables in the database"""
+        conn = sqlite3.connect("test_database.db")
+        cursor = conn.cursor()
+
+        try: # try except ensures that data will only be deleted if it succeeds in deleting the specific AMR from ALL tables, else nothing is deleted
+            conn.execute("BEGIN")
+
+            cursor.execute("DELETE FROM amr WHERE id = ?", (id,))
+            cursor.execute("DELETE FROM data WHERE id = ?", (id,))
+            cursor.execute("DELETE FROM error WHERE id = ?", (id,))
+        
+            conn.commit()
+
+        except Exception as e:
+            conn.rollback()
+            print("Error: ", e)
+        
+        finally:
+            conn.close()
+
+    # Jeg antager at det bare er alt i "data" table i databasen. Kan nemt rettes, hvis nødvendigt.
+    def save_network_data(self, id, amr, timestamp, rtt, jitter, packet_loss, signal_strength, noise, rssi, battery, pos_x, pos_y):
+        """Saves all network data to database"""
         conn = sqlite3.connect("test_database.db")
         cursor = conn.cursor()
 
         cursor.execute(
-            "DELETE FROM amr WHERE id = ?", (id,)
+            "INSERT INTO data (id, amr_, timestamp, rtt, jitter, packet_loss, signal_strength, noise, rssi, battery, pos_x, pos_y)",
+            (id, amr, timestamp, rtt, jitter, packet_loss, signal_strength, noise, rssi, battery, pos_x, pos_y)
         )
 
         conn.commit()
         conn.close()
 
-    def save_network_data(self): # Bør nok først laves, når vi har en måde at få data på
-        pass
+    # Jeg antager at det her er alt der skal i "error" table.
+    def save_amr_status_log(self, id, amr, timestamp, error, error_desc):
+        """Saves status of amr to database"""
+        conn = sqlite3.connect("test_database.db")
+        cursor = conn.cursor()
+        
+        cursor.execute(
+            "INSERT INTO error (id, amr_, timestamp, error, error_desc)", 
+            (id, amr, timestamp, error, error_desc)
+        )
 
-    def save_amr_status_log(self): # Er lidt i tvivl om den her er nødvendig
-        pass
+        conn.commit()
+        conn.close()
