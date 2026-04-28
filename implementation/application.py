6bbf136a9077c02924767a1c69be958a0e013eec
jahrkob <95243077+jahrkob@users.noreply.github.com>
2026-04-28 14:31:10 +0200

    Added code to application.py that is creating a networkmonitorer object, creating an amr object and testing whether adding an amr to the database works.

--- implementation/application.py
+++ implementation/application.py
@@ -1,2 +1,27 @@
 from network_monitorer import *
+from amr import *
+from data_grapher import *
+from internet_device import *
+from raspberry_pi_files.RaspberryPi import *
 
+if __name__ == "__main__":
+    monitor = NetworkMonitorer(
+        fleet_manager_ip="192.168.1.1",
+        database="database_files/instance/database.db",
+        auth_token="Basic ZGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA"
+    )
+
+    amr_1 = AMR(
+        id=1, 
+        amr_ip="192.168.0.100", 
+        name="AMR TEST OBJECT", 
+        raspi_ip="192.168.1.101"
+    )
+
+    monitor.add_amr_to_database(
+        ip="192.168.0.100",
+        name="AMR #1",
+        raspi_ip="192.168.1.101"
+    )
+
+    print(monitor)
