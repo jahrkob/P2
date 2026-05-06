05cce1718dab89741808e2f6a232f1047e72783d
Lifehunted <196421221+Lifehunted@users.noreply.github.com>
2026-05-06 10:31:29 +0200

    some changes

--- implementation/raspberry_pi_files/API_raspberry_pi.py
+++ implementation/raspberry_pi_files/API_raspberry_pi.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
 from datetime import datetime
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+
+interface_to_monitor = 'wlan0'
 
 app = Flask(__name__)
 jwt = JWTManager(app)
 api = Api(app)
 
-app.config['JWT_SECRET_KEY'] = 'distributor'
+app.config['JWT_SECRET_KEY'] = 'distributor_distributor_distributor'
 
 # ##### creating api key #####
 # with app.app_context():
@@ -22,31 +24,35 @@ statusFields = { # for requesting which AMR's there are
     'noise':fields.Float
 }
 
-
 class Status(Resource):
     @jwt_required()
     @marshal_with(statusFields)
     def get(self): # request status
-        return get_wireless_info()
+        return get_wireless_info(interface_to_monitor)
 
 
 def get_wireless_info(interface="wlan0"):
     with open("/proc/net/wireless") as f:
         lines = f.readlines()
 
+        if not lines[2:]:
+            print(f'could not find match for "{interface}" in "/proc/net/wireless"')
+            abort(404)
+
     for line in lines[2:]:
-        if line.strip().startswith(interface):
-            parts = line.split()
+        line.strip().startswith(interface)
+        parts = line.split()
 
-            quality = float(parts[2])
-            signal = float(parts[3])
-            noise = float(parts[4])
+        quality = float(parts[2])
+        signal = float(parts[3])
+        noise = float(parts[4])
 
-            return {
-                "rssi": signal,
-                "quality": quality,
-                "noise": noise if noise != -256.0 else None
-            }
+        return {
+            "rssi": signal,
+            "quality": quality,
+            "noise": noise if noise != -256.0 else None
+        }
+            
 
 api.add_resource(Status, '/api/status')
 
@@ -54,5 +60,16 @@ api.add_resource(Status, '/api/status')
 def home():
     return '<h1>Flask REST API</h1>'
 
+
+
 if __name__ == '__main__':
-     app.run(debug=True,host='0.0.0.0')
+    @app.before_request
+    def log_request_info():
+        print("----- Incoming Request -----")
+        print("Method:", request.method)
+        print("URL:", request.url)
+        print("Headers:", dict(request.headers))
+        print("Body:", request.get_data())
+        print("----------------------------")
+
+    app.run(debug=True,host='0.0.0.0')
--- implementation/raspberry_pi_files/RaspberryPi.py
+++ implementation/raspberry_pi_files/RaspberryPi.py
@@ -1,7 +1,6 @@
 import requests
 from typing import Optional, TypedDict
-# from internet_device import InternetDevice
-from implementation.internet_device import InternetDevice
+from internet_device import InternetDevice
 
 class SignalData(TypedDict): # So pylance knows what each key in json has as value
     rssi: float
@@ -10,10 +9,10 @@ class SignalData(TypedDict): # So pylance knows what each key in json has as val
 
 class RaspberryPi(InternetDevice):
     def __init__(self, device_name, ip, port=80):
-        super().__init__(device_name, ip)
+        super().__init__(ip=ip, device_name=device_name)
         self.port = port
         self.__api_key = {
-            "Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc3NzAzMDA4NSwianRpIjoiMmQ4YmRjMzUtOWU5ZC00NjdiLWIzZmEtMDA5ODBmYTY3NTBmIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImRpc3RyaWJ1dG9yIiwibmJmIjoxNzc3MDMwMDg1LCJjc3JmIjoiZWQ1NTkxNmQtNDg3OS00NjZmLTlhMDctZGJlY2Y0N2Y0OTM3In0.-ndt5YYzRl7YvSUk76CSOsn163Plx9NTYEYf0YRneNs'
+            "Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc3ODA1MTMwMywianRpIjoiMmJiYTUyMjEtZWE2Ni00ZjRmLTk5ZDgtYTIwYjNjZDQzNTRlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImRpc3RyaWJ1dG9yIiwibmJmIjoxNzc4MDUxMzAzLCJjc3JmIjoiODQ2OGQzYjktYWEyYi00OWQ2LWIzMjEtZTZiYzJiYWUyY2YwIn0.UTr3yRfM3xxwJzOQ63YNGEHyFJDIpj8bKpnnL-7-m3I'
         }
 
     def get_signal_metrics(self) -> SignalData:
@@ -24,20 +23,18 @@ class RaspberryPi(InternetDevice):
                 signal_strength (float)
                 noise (Optional[float]): Noise level, null if unavailable.
         """
-        api_response = requests.get(f'http://{self.ip}:{self.port}/api/status',headers=self.__api_key)
+        url = f'http://{self.ip}:{self.port}/api/status'
+        print(f'{self}: get {url}')
+        api_response = requests.get(url,headers=self.__api_key)
         api_response.raise_for_status() # Raises an HTTPError if the response was an error
 
-        rssi = api_response.json().get('rssi')
-        signal_strength = api_response.json().get('signal_strength')
-        noise = api_response.json().get('noise')
-
-        return api_response.json(), rssi, signal_strength, noise
+        return api_response.json()
 
 if __name__ == "__main__":
     ##### TESTING #####
     name = 'testing_rasp' # can be whatever
-    ip = '192.168.0.95'
+    ip = '192.168.0.96'
     port = 5000 # Only 5000 for testing in reality should be 80
-    rasp = RaspberryPi(name,ip,port)
+    rasp = RaspberryPi(device_name=name,ip=ip,port=port)
 
     print(rasp.get_signal_metrics())
