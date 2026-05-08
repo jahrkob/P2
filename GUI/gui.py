b1c573be6afd7f3000f9a7149b197267f1e7f603
Lifehunted <196421221+Lifehunted@users.noreply.github.com>
2026-05-08 09:24:31 +0200

    start to amr_overview

--- GUI/gui.py
+++ GUI/gui.py
@@ -266,3 +266,13 @@ class GUI(ctk.CTk):
         if self.update_loop_id is not None:
             self.after_cancel(self.update_loop_id)
         self.destroy()
+
+
+
+# ==================================================
+#                   Lasse Stuff
+# ==================================================
+
+class amr_card(ctk.CTkFrame):
+    def __init__(self, master, width = 200, height = 200, corner_radius = None, border_width = None, bg_color = "transparent", fg_color = None, border_color = None, background_corner_colors = None, overwrite_preferred_drawing_method = None, **kwargs):
+        super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors, overwrite_preferred_drawing_method, **kwargs)
