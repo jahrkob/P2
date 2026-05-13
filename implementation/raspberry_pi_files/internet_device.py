5273b4c99aca4f9e45aad9f4bdb356a0cbbb1e3f
VM <vm@local>
2026-05-13 12:05:19 +0000

    Removed double file

--- implementation/raspberry_pi_files/internet_device.py
+++
@@ -1,9 +0,0 @@
-class InternetDevice:
-    """Base class for all internet-connected devices."""
-
-    def __init__(self, ip, device_name=""):
-        self.device_name = device_name
-        self.ip = ip # ip address
-
-    def __str__(self):
-        return f"{self.device_name} ({self.ip})"
